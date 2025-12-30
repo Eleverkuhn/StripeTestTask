e8d7097950e67411957bd8350c32b0fb6e847251
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-12-30 10:30:02 +0300

    feat(project-setup): Add custom logging

---
+++ payments/logger/setup.py
@@ -0,0 +1,23 @@
+import json, logging
+from logging import Logger
+
+from config import BASE_DIR
+
+
+class LoggingConfig:
+    path = BASE_DIR.joinpath("logger", "config.json")
+    logger_name = "payments"
+
+    @property
+    def logger(self) -> Logger:
+        return logging.getLogger(self.logger_name)
+
+    def load(self) -> None:
+        with open(self.path) as json_config:
+            config_file = json.load(json_config)
+        return config_file
+
+
+class OnlyInfoFilter(logging.Filter):
+    def filter(self, record):
+        return record.levelno == logging.INFO
