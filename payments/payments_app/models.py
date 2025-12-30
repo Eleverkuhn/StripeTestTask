b4fb99a92b44c31ef2f4e97a56a7de6402ef01e4
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-12-30 11:05:01 +0300

    feat(item): Add 'items' table
    - Add Item DjangoORM model
    - Apply migrations
    - Add fixture data

--- payments/payments_app/models.py
+++ payments/payments_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=80, validators=[MinLengthValidator(1)])
+    description = models.CharField(max_length=100, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    class Meta:
+        db_table = "items"
+
+    def __repr__(self) -> str:
+        return f"{self.name}"
