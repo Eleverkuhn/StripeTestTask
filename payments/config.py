65519b29fc8aae8aab879fc029a86083c817cb16
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-12-30 10:30:15 +0300

    feat(project-setup): Add loading env variables to the project

---
+++ payments/config.py
@@ -0,0 +1,35 @@
+import os
+
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent
+
+
+def get_env_file() -> str:
+    dir = Path(__file__).parent.parent
+    if os.getenv("TEST_ENV") == "docker":
+        return str(dir / ".env.docker")
+    return str(dir / ".env.local")
+
+
+class Settings(BaseSettings):
+    test_env: str
+
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_host: str
+    postgres_port: str
+
+    django_host: str
+    django_port: str
+    django_key: str
+
+    pythonpath: str
+
+    model_config = SettingsConfigDict(env_file=get_env_file())
+
+
+settings = Settings()
