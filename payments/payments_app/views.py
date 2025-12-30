504928e9d03d07c015a527f45415fef2386a9101
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-12-30 18:31:13 +0300

    fix(item): Add seperate method for generating context in ItemView. Change response object to JsonResponse in BuyView

--- payments/payments_app/views.py
+++ payments/payments_app/views.py
@@ -1,10 +1,12 @@
 from typing import override
 
-from django.http.response import HttpResponse
+from django.http import JsonResponse
 from django.views import View
 from django.views.generic import TemplateView
 
+from config import settings
 from payments_app.services import ItemService, BuyService
+from payments_app.models import Item
 
 
 class ItemView(TemplateView):
@@ -14,15 +16,20 @@ class ItemView(TemplateView):
     def get_context_data(self, id: int, *args, **kwargs):
         context = super().get_context_data(**kwargs)
         item = ItemService().get_item(id)
-        context["item"] = [{"name": field.verbose_name,
-                            "value": getattr(item, field.name)}
+        return self._generate_context(context, item)
+
+    def _generate_context(self, context: dict, item: Item) -> dict:
+        context["stripe_pk"] = settings.stripe_pk
+        context["item_id"] = item.id
+        context["title"] = item.name
+        context["item"] = {field.verbose_name: getattr(item, field.name)
                            for field
-                           in item._meta.fields]
+                           in item._meta.fields}
         return context
 
 
 class BuyView(View):
-    def get(self, request, id: int, *args, **kwargs):
+    def get(self, request, id: int, *args, **kwargs) -> JsonResponse:
         item = ItemService().get_item(id)
         session = BuyService(item).generate_stripe_session()
-        return HttpResponse(session.id)
+        return JsonResponse({"session_id": session.id})
