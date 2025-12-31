d6aa9a4b39c28e68855cedd1f0de4aaa96438005
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-12-31 11:10:20 +0300

    fix(project-setup): Change seed_db.sh

--- seed_db.sh
+++ seed_db.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-
-echo "Waiting for PostgreSQL at $POSTGRES_HOST:$POSTGRES_PORT..."
-
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  sleep 1
-done
-
 python manage.py migrate
 python manage.py loaddata items.json
 exec "$@"
