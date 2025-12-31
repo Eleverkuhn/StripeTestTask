a5baf9bbb9428c2500ee54bfb2542ad106b36979
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-12-31 08:28:01 +0300

    feat(item): Add display of item name. Add min value constraint to price field. Apply migrations

--- payments/payments_app/models.py
+++ payments/payments_app/models.py
@@ -1,14 +1,21 @@
+from decimal import Decimal
+
 from django.db import models
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, MinValueValidator
 
 
 class Item(models.Model):
     name = models.CharField(max_length=80, validators=[MinLengthValidator(1)])
     description = models.CharField(max_length=100, null=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=8,
+                                decimal_places=2,
+                                validators=[MinValueValidator(Decimal("1.00"))])
 
     class Meta:
         db_table = "items"
 
     def __repr__(self) -> str:
+        return f"{self.name}"
+
+    def __str__(self) -> str:
         return f"{self.name}"
