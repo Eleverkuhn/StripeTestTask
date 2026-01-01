c167b3ad29cdca9c85e7d9b6c89771da7f359862
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2026-01-01 17:41:27 +0300

    feat(project-setup): Add whitenoise lib for serving static files

--- payments/settings.py
+++ payments/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 logging_config = LoggingConfig().load()
 logging.config.dictConfig(logging_config)
--- seed_db.sh
+++ seed_db.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 python manage.py migrate
 python manage.py loaddata items discounts taxes orders
+python manage.py collectstatic --noinput
 PYTHONPATH=./payments python create_admin.py
 exec "$@"
