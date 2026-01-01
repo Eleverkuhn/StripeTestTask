497b5bd41cb6a5c802c54b2d51401aaea6e35e33
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2026-01-01 10:46:50 +0300

    feat(order): Add Order to Django admin panel

--- payments/payments_app/admin.py
+++ payments/payments_app/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from payments_app.models import Item
+from payments_app.models import Item, Order
 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     fields = ["id", "name", "description", "price"]
+    readonly_fields = ["id"]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    fields = ["id", "items"]
     readonly_fields = ["id"]
