ec1d8b6a303466395174107504b0bd152e048282
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2026-01-01 15:50:41 +0300

    feat(price): Add calculation of Discount and Tax to Stripe checkout form

--- payments/payments_app/services.py
+++ payments/payments_app/services.py
@@ -1,7 +1,7 @@
 from typing import override
 
 import stripe
-from stripe import Price
+from stripe import Price, Coupon, TaxRate
 from stripe.checkout import Session
 
 from config import settings
@@ -14,12 +14,17 @@ class BaseCheckoutService:
     def __init__(self) -> None:
         stripe.api_key = settings.stripe_sk
         stripe.api_version = "2025-12-15.clover"
+        self.session_data = {"ui_mode": "custom",
+                             "mode": "payment",
+                             "return_url": self.domain}
 
-    def construct_stripe_session(self, line_items: list[dict]) -> Session:
-        return stripe.checkout.Session.create(ui_mode="custom",
-                                              line_items=line_items,
-                                              mode="payment",
-                                              return_url=self.domain)
+    def construct_stripe_session(self,
+                                 line_items: list[dict],
+                                 coupon: Coupon | None = None) -> Session:
+        self.session_data["line_items"] = line_items
+        if coupon:
+            self.session_data["discounts"] = [{"coupon": coupon.id}]
+        return stripe.checkout.Session.create(**self.session_data)
 
 
 class CheckoutOrderService(BaseCheckoutService):
@@ -30,13 +35,45 @@ class CheckoutOrderService(BaseCheckoutService):
 
     def generate_stripe_session(self) -> Session:
         line_items = self.generate_line_items()
-        session = self.construct_stripe_session(line_items)
+        coupon = self.generate_coupon()
+        session = self.construct_stripe_session(line_items, coupon)
         return session
 
     def generate_line_items(self) -> list[dict]:
-        return [CheckoutItemService(item).construct_line_item()
-                for item
-                in self.order.items.all()]
+        line_items = []
+        tax = self.generate_tax()
+
+        for item in self.order.items.all():
+            self.populate_line_items(line_items, item, tax)
+
+        return line_items
+
+    def populate_line_items(self,
+                            line_items: list,
+                            item: Item,
+                            tax: TaxRate | None) -> None:
+        line_item = CheckoutItemService(item).construct_line_item()
+        self.add_tax(line_item, tax)
+        line_items.append(line_item)
+
+    def add_tax(self, line_item: dict, tax: TaxRate | None) -> None:
+        if tax:
+            line_item["tax_rates"] = [tax.id]
+
+    def generate_coupon(self) -> Coupon | None:
+        if self.order.discount:
+            coupon = stripe.Coupon.create(percent_off=self.order.discount.value,
+                                          duration="once")
+            return coupon
+
+    def generate_tax(self) -> TaxRate | None:
+        if self.order.tax:
+            tax = stripe.TaxRate.create(display_name="НДС",
+                                        description="Налог на добавочную стоимость",
+                                        jurisdiction="RU",
+                                        percentage=self.order.tax.value,
+                                        inclusive=False)
+            return tax
 
 
 class CheckoutItemService(BaseCheckoutService):
